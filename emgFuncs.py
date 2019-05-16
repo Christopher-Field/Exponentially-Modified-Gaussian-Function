6f9d2f286241d0c34c05b55492876b1cc27e8d8a
Christopher T. Field <christopher.t.field@nasa.gov>
2019-05-16 09:35:17 -0400

    Added __init__.py so package can be imported. Put doc strings in standard formats.

--- emgFuncs.py
+++ emgFuncs.py
@@ -1,6 +1,16 @@
-# Exponentially-Modified Gaussian functions for Python
+"""
+Exponentially-Modified Gaussian functions for Python
+
+Used  in
+*Characterizing the System Impulse Response Function From Photon-Counting LiDAR Data*
+by Adam P. Greeley , Thomas A. Neumann, Nathan T. Kurtz, Thorsten Markus, and Anthony J. Martino
+to be published in **IEEE TRANSACTIONS ON GEOSCIENCE AND REMOTE SENSING**.
+
+Digital Object Identifier 10.1109/TGRS.2019.2907230
+
+The regular entry point is **exgausspdf()**.
+"""
 # April 30, 2019
-#
 #
 
 import numpy as np
@@ -9,10 +19,16 @@ from scipy.special import erfc
 
 
 
-#-- PURPOSE: compute the cumulative probability for a normalized Gaussian
 def pnf(x):
-	#-- x is scalar or array
-	#-- function returns probability with same shape as x
+	"""Helper function to compute the cumulative probability for a normalized Gaussian
+
+	:param x: x is scalar or array. Note that x is overwritten by the function evaluation.
+	:return: probability with same shape as x
+
+	The pnf is defined as the probability that x<X when X is Gaussian distributed.
+	Note that for numerical stability, a different formula is used for
+	positive and negative arguments.
+	"""
 	a = x < 0
 	b = x >= 0
 	p = x
@@ -23,12 +39,16 @@ def pnf(x):
 
 
 
-#-- PURPOSE: compute exponentially-modified Gaussian PDF
 def exgausspdf(x,mu,sig,tau):
+	"""Compute exponentially-modified Gaussian PDF with parameters mu, sig, and tau.
 
-	#-- Given ex-Gaussian parameters mu, sig, tau, return density at x. 
-	#-- mu, sig, tau are scalar; x is either scalar or array.
-	#-- function returns density with same shape as x
+	:param x: x is scalar or array of values at which to compute the density
+	:param mu: scalar: central tendency
+	:param sig: scalar: symmetric variablity
+	:param tau: scalar: Exponential decay
+	:return: probability with same shape as x
+
+	"""
 	arg1 = (mu / tau) + ((sig**2) / (2 * (tau**2)) ) - (x / tau)
 	arg2 = ((x - mu) - ((sig**2) / tau)) / sig
 	f = (1 / tau) * np.exp(arg1) * pnf(arg2)
